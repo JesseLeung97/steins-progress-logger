549406064630664b980fa988d31b652521439c82
Jesse Leung <jesse@steins.co.jp>
2022-11-11 11:24:43 +0900

    [Update] Change to real logging function

--- google_sheets_reader/src/slack_client.py
+++ google_sheets_reader/src/slack_client.py
@@ -5,6 +5,6 @@ from google_sheets_reader.environment_variables import environment_variables
 # Send update message in clack channel
 def send_message(message: str) -> None:
     client = WebClient(environment_variables.BOT_TOKEN)
-    res = client.chat_postMessage(channel=environment_variables.CHANNEL_ID, text="Lambda test.")
+    res = client.chat_postMessage(channel=environment_variables.CHANNEL_ID, text=message)
 
     print(f"{res}")
